1fb73b2285fe8d3b8af6f92bc35a9d5070215dac
Frostyy890 <abdurahmon2004nuriy@gmail.com>
2024-07-10 20:47:42 +0500

    feat: added script for running drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "src/config";
 
 export default defineConfig({
   dialect: "mysql",
   out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
+  dbCredentials: {
+    host: config.db.host,
+    port: config.db.port,
+    user: config.db.user,
+    password: config.db.password,
+    database: config.db.database,
+  },
 });
