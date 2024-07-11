95d20c590e9ea382ee572714951b49aad0ca2a3a
Frostyy890 <abdurahmon2004nuriy@gmail.com>
2024-07-11 10:14:58 +0500

    refactor: removed unused vars feat: stricter tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
 import express from "express";
 import { config } from "./config";
-import { UserController, catchAsync } from "@v1/utils";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/", (_req: express.Request, res: express.Response) => {
   res.send("Hello World!");
 });
-
-const userController = new UserController();
-
-app.get("/users", catchAsync(userController.findMany.bind(userController)));
 
 app.listen(config.server.port, () =>
   console.log(
