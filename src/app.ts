c601a11994d2686f2cea5e44734ecf791123af08
Abdurahmon <abdurahmon2004nuriy@gmail.com>
2024-07-11 14:58:37 +0500

    refactor: imporved docker configuration

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_req: express.Request, res: express.Response) => {
-  res.send("Hello World!");
+  res.send("Hello World");
 });
 
 app.listen(config.server.port, () =>
